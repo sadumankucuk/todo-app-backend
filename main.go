bb6a5cba13d969c8d70b506e26c0903108ac206c
sadumankucuk <sadumankucuk@gmail.com>
2022-02-26 23:02:17 +0300

    implement router

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"todo/handler"
+	"todo/repository"
+	"todo/service"
+)
 
 func main() {
-	fmt.Println("hello")
+	repository := repository.NewITodoRepository()
+	service := service.NewITodoService(repository)
+	handler := handler.NewITodoHandler(service)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/v1/todos", handler.CreateTodo).Methods(http.MethodPost)
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
